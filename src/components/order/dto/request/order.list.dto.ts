a556a30a4fdab7680b303ac88b01b21969850974
wonjjee <yoteamo7@naver.com>
2024-01-13 15:02:46 +0900

    feat: added OrderListDto

---
+++ src/components/order/dto/request/order.list.dto.ts
@@ -0,0 +1,26 @@
+import { Transform } from 'class-transformer';
+import { IsDate, IsNumber, IsOptional } from 'class-validator';
+import * as moment from 'moment-timezone';
+
+export class OrderListDto {
+  @Transform(({ value }) => moment(value).toDate())
+  @IsDate()
+  startDate: Date;
+
+  @Transform(({ value }) => moment(value).toDate())
+  @IsDate()
+  endDate: Date;
+
+  @IsNumber()
+  orderType = 2;
+
+  @IsOptional()
+  @IsNumber()
+  customerId?: number;
+
+  @IsNumber()
+  pageSize = 50;
+
+  @IsNumber()
+  pageNo = 1;
+}
