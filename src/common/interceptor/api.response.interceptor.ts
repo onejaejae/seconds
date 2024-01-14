a432bf811df46800d90dee2531cc65cbf978768a
wonjjee <yoteamo7@naver.com>
2024-01-14 11:58:19 +0900

    feat: added ApiResponseInterceptor

---
+++ src/common/interceptor/api.response.interceptor.ts
@@ -0,0 +1,17 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+@Injectable()
+export class ApiResponseInterceptor implements NestInterceptor {
+  intercept(
+    _context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(map((data) => ({ message: 'success', data })));
+  }
+}
--- src/setNest.app.ts
+++ src/setNest.app.ts
@@ -5,9 +5,11 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { TypeORMExceptionFilter } from './common/filter/typeorm.exception.filter';
+import { ApiResponseInterceptor } from './common/interceptor/api.response.interceptor';
 
 export function setNestApp<T extends INestApplication>(app: T): void {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(new ApiResponseInterceptor());
   app.useGlobalFilters(new TypeORMExceptionFilter());
 }
